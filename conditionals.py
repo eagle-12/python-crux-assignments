fc822a8711d63244f92c07165337ab156c6a530f
eagle-12 <nikhil29091997@gmail.com>
2017-07-09 19:22:39 +0530

    assignment

---
+++ conditionals.py
@@ -0,0 +1,6 @@
+import random
+random_number = random.randint(1,20)
+if(random_number%2==1):
+	print "the number {} is odd".format(random_number)
+else:
+	print "the number {} is even".format(random_number)	
